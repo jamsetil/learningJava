b9971e95d6a3442768ce2b4e1745bd2303eb8a85
Ilyas <iazizzade64@gmail.com>
2023-09-22 17:12:03 +0400

    movie api

---
+++ src/main/java/az/moviesapiilyas/moviesapi/MoviesapiApplication.java
@@ -0,0 +1,16 @@
+package az.moviesapiilyas.moviesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class MoviesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesapiApplication.class, args);
+	}
+
+
+}
